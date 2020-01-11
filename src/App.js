cd8da78205212b9e2547f8be16e82ed6130e1be4
Bobur <bobur.hu0991@gmail.com>
2020-01-10 22:14:43 -0500

    app init setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./components/Home";
+import UserProfile from "./components/UserProfile";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      accountBalance: 14568.27,
+      currentUser: {
+        userName: "bob_hu",
+        memberSince: "4/20/69"
+      }
+    }
+  };
+
+  render() {
+    const HomeComponent = () => (<Home accountBalance={this.state.accountBalance}/>);
+    const UserProfileComponent = () => (<UserProfile userName={this.state.currentUser.userName} 
+                              memberSince={this.state.currentUser.memberSince}/>);
+    return(
+      <Router>
+        <div>
+          <Route exact path="/" render={HomeComponent}/>
+          <Route exact path="/userProfile" render={UserProfileComponent}/>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
