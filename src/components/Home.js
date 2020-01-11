dccdd1c9352cddf78da1ab0fefd2f973b5e19c98
Bobur <bobur.hu0991@gmail.com>
2020-01-10 21:58:48 -0500

    home setup

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import AccountBalance from "./AccountBalance";
+
+class Home extends React.Component {
+    render() {
+        return(
+            <div>
+                <img src="https://letstalkpayments.com/wp-content/uploads/2016/04/Bank.png" alt="bank"/>
+                <h1>Bank of React</h1>
+                <AccountBalance accountBalance={this.props.accountBalance}/>
+            </div>
+        );
+    }
+}
+
+export default Home;
